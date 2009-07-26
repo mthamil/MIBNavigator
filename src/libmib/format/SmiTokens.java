87eccdc978564078793cef0098949685b229e1eb
Matthew Hamilton <matthamilton@live.com>
2009-07-26 22:07:41 +0000

    Updated license text.
    git-svn-id: https://svn.code.sf.net/p/mibnavigator/code/mibnavigator@41 04391ee8-9fb0-45f4-8207-3b47ca054329

--- src/libmib/format/SmiTokens.java
+++ src/libmib/format/SmiTokens.java
@@ -1,7 +1,7 @@
 /**
  * libmib - Java SNMP Management Information Base Library
  *
- * Copyright (C) 2008, Matt Hamilton <mhamilton2383@comcast.net>
+ * Copyright (C) 2009, Matt Hamilton <matthamilton@live.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
