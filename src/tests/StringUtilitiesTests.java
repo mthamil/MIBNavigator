bb2d9beb5d3b870e8d4ef1a8dc99c246832c8737
Matthew Hamilton <matthamilton@live.com>
2009-08-23 18:54:12 +0000

    Added some utilities unit tests
    git-svn-id: https://svn.code.sf.net/p/mibnavigator/code/mibnavigator@57 04391ee8-9fb0-45f4-8207-3b47ca054329

---
+++ src/tests/StringUtilitiesTests.java
@@ -0,0 +1,88 @@
+/**
+ * MIB Navigator
+ *
+ * Copyright (C) 2009, Matt Hamilton <matthamilton@live.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package tests;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import utilities.StringUtilities;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class StringUtilitiesTests extends TestCase
+{
+	public static Test suite()
+	{
+		return new TestSuite(StringUtilitiesTests.class);
+	}
+	
+	public void testEmptyJoin() 
+	{
+		List<String> list = new ArrayList<String>();
+		String joinedList = StringUtilities.join(list, ",");
+		assertEquals("", joinedList);
+	}
+	
+	public void testSingleElementJoin()
+	{
+		List<String> list = Arrays.asList("test1");
+		String joinedList = StringUtilities.join(list, ",");
+		assertEquals("test1", joinedList);
+	}
+
+	public void testMultiElementJoin()
+	{
+		List<String> list = Arrays.asList("test1", "test2");
+		String joinedList = StringUtilities.join(list, ",");
+		assertEquals("test1,test2", joinedList);
+	}
+		
+	public void testArrayJoin()
+	{
+		String[] array = new String[] { "test1", "test2", "test3" };
+		String joinedList = StringUtilities.join(array, "+");
+		assertEquals("test1+test2+test3", joinedList);
+	}
+	
+	public void testSublistJoin()
+	{
+		List<String> list = Arrays.asList("1", "2", "3", "4", "5");
+		int maxItems = Math.min(list.size(), 3);
+		String joinedSublist = StringUtilities.join(list.subList(0, maxItems), ",");
+		assertEquals("1,2,3", joinedSublist);
+		
+		maxItems = Math.min(list.size(), 8);
+		joinedSublist = StringUtilities.join(list.subList(0, maxItems), ",");
+		assertEquals("1,2,3,4,5", joinedSublist);
+		
+		maxItems = Math.min(list.size(), 0);
+		joinedSublist = StringUtilities.join(list.subList(0, maxItems), ",");
+		assertEquals("", joinedSublist);
+		
+		list = new ArrayList<String>();
+		maxItems = Math.min(list.size(), 3);
+		joinedSublist = StringUtilities.join(list.subList(0, maxItems), ",");
+		assertEquals("", joinedSublist);
+	}
+}
