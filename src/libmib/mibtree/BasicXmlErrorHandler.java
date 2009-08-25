ef034127a38fc17b0fe8decdbdeec196f32f7cb4
Matthew Hamilton <matthamilton@live.com>
2009-08-25 06:55:08 +0000

    Re-add file that was actually necessary.
    git-svn-id: https://svn.code.sf.net/p/mibnavigator/code/mibnavigator@61 04391ee8-9fb0-45f4-8207-3b47ca054329

---
+++ src/libmib/mibtree/BasicXmlErrorHandler.java
@@ -0,0 +1,69 @@
+/**
+ * libmib - Java SNMP Management Information Base Library
+ *
+ * Copyright (C) 2005, Matt Hamilton <matthew.hamilton@washburn.edu>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package libmib.mibtree;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ * This class reports all types of XML parsing errors by re-throwing the
+ * passed exception.  It is useful for halting parsing if any kind of 
+ * problem occurs, even if it is supposedly recoverable.
+ * 
+ */
+public class BasicXmlErrorHandler implements ErrorHandler 
+{
+
+    /**
+     * 
+     */
+    public BasicXmlErrorHandler() 
+    {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#warning(org.xml.sax.SAXParseException)
+     */
+    public void warning(SAXParseException e) throws SAXException 
+    {
+        throw e;
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#error(org.xml.sax.SAXParseException)
+     */
+    public void error(SAXParseException e) throws SAXException 
+    {
+        throw e;
+    }
+
+    /* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#fatalError(org.xml.sax.SAXParseException)
+     */
+    public void fatalError(SAXParseException e) throws SAXException 
+    {
+        throw e;
+    }
+
+}
